e04ddbd7758e905bb0eda7db8d3874fe1a91b879
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2019-04-19 21:59:58 -0300

    feat: Apresentação view novo baralho

--- UdacityMobileFlashcards/Views/NovoBaralhoView.js
+++ UdacityMobileFlashcards/Views/NovoBaralhoView.js
@@ -1,15 +1,50 @@
 import React, { Component } from 'react'
-import { View, Text, Platform, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, TextInput, Button } from 'react-native'
 
 class NovoBaralhoView extends Component {
+
+    state = {
+        tituloBaralho: ''
+    }
+
+    salvar = () => {
+
+        // Validar titulo do baralho
+
+        // salvar novo baralho
+
+        // Redirecionar
+        this.props.navigation.navigate('Listagem')
+    }
 
     render() {
         return (
             <View>
-                <Text>Novo baralho</Text>
+                <Text style={styles.tituloPergunta}>Qual o título do seu novo baralho?</Text>
+
+                <TextInput
+                    style={styles.inputTitulo}
+                    onChangeText={(tituloBaralho) => this.setState({ tituloBaralho })}
+                    value={this.state.tituloBaralho}
+                />
+
+                <Button onPress={this.salvar} title="Salvar" />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    tituloPergunta: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 28,
+    },
+    inputTitulo: {
+        height: 20,
+        borderColor: 'gray',
+        borderWidth: 1
+    }
+})
 
 export default NovoBaralhoView
