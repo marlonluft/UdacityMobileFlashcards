14c1a2935ad292251127b090d5bff68cf56b9d7a
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2019-04-19 21:43:50 -0300

    feat: Apresentação view de baralho

--- UdacityMobileFlashcards/Views/BaralhoView.js
+++ UdacityMobileFlashcards/Views/BaralhoView.js
@@ -1,16 +1,55 @@
 import React, { Component } from 'react'
-import { View, Text, Platform, StyleSheet, Button  } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 
 class BaralhoView extends Component {
 
+    state = {
+        descricao: '',
+        qtdPerguntas: 0,
+        id: -1
+    }
+
+    componentDidMount() {
+        const { id } = this.props.navigation.state.params
+
+        this.setState({
+            descricao: 'Baralho View',
+            qtdPerguntas: 2,
+            id: id
+        })
+
+    }
+
     render() {
+
+        const { descricao, qtdPerguntas, id } = this.state
+
         return (
-            <View>
-                <Text>Baralho</Text>
-                <Button onPress={() => this.props.navigation.navigate('Listagem')} title="Listagem"/>
+
+            <View style={styles.container}>
+                <Text style={styles.descricao}>{descricao}</Text>
+                <Text style={styles.perguntas}>{qtdPerguntas > 0 ? qtdPerguntas + ' pergunta(s)' : 'Nenhuma pergunta cadastrada'}</Text>
+                <Button onPress={() => this.props.navigation.navigate('NovaPergunta', { id })} title="Nova Pergunta" />
+                <Button disabled={qtdPerguntas === 0} onPress={() => this.props.navigation.navigate('Quiz', { id })} title="Começar Quiz" />
             </View>
         )
     }
 }
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1
+    },
+    descricao: {
+        textAlign: 'center',
+        fontWeight: 'bold',
+        fontSize: 28,
+    },
+    perguntas: {
+        textAlign: 'center',
+        fontSize: 20,
+        color: 'gray'
+    }
+})
 
 export default BaralhoView
