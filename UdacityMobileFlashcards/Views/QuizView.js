8f5ba7e05faddb9b4475a50412c810e6e4e7fbdd
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2019-04-19 22:22:26 -0300

    feat: Apresentação view Quiz

--- UdacityMobileFlashcards/Views/QuizView.js
+++ UdacityMobileFlashcards/Views/QuizView.js
@@ -1,12 +1,58 @@
 import React, { Component } from 'react'
-import { View, Text, Platform, StyleSheet } from 'react-native'
+import { View, Text, StyleSheet, Button } from 'react-native'
 
 class QuizView extends Component {
 
+    state = {
+        qtdQuestionado: 0,
+        qtdTotal: 0,
+        pergunta: '',
+        resposta: '',
+        exibirPergunta: true
+    }
+
+    componentDidMount() {
+        const { id } = this.props.navigation.state.params
+
+        this.setState({
+            pergunta: 'Teste pergunta',
+            resposta: 'Teste resposta',
+            qtdTotal: 10
+        })
+
+    }
+
+    acertei = () => {
+
+        this.setState(oldstate => ({
+            qtdQuestionado: oldstate.qtdQuestionado + 1,
+        }))
+    }
+
+    errei = () => {
+
+        this.setState(oldstate => ({
+            qtdQuestionado: oldstate.qtdQuestionado + 1,
+        }))
+    }
+
     render() {
+
+        const { qtdQuestionado, qtdTotal, pergunta, resposta, exibirPergunta } = this.state
+
         return (
             <View>
-                <Text>Quiz</Text>
+                <Text>{qtdQuestionado + '/' + qtdTotal}</Text>
+                {
+                    exibirPergunta ?
+                        <Text style={{}}>{pergunta}</Text>
+                        : <Text>{resposta}</Text>
+                }
+
+                <Button onPress={() => this.setState({ exibirPergunta: !exibirPergunta })} title={exibirPergunta ? 'Exibir Resposta' : 'Exibir Pergunta'} />
+
+                <Button onPress={() => this.acertei()} title="Acertei" />
+                <Button onPress={() => this.errei()} title="Errei" />
             </View>
         )
     }
