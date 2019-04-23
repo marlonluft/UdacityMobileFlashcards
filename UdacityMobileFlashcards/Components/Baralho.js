f46255ea488dc59dc2e03c0ca9d57bed500ac8c1
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2019-04-22 22:35:56 -0300

    feat: Estilo baralho na listagem de baralhos

--- UdacityMobileFlashcards/Components/Baralho.js
+++ UdacityMobileFlashcards/Components/Baralho.js
@@ -17,8 +17,8 @@ export default function Baralho({ dados, exibirBaralho }) {
     return (
         <TouchableOpacity onPress={onBaralhoClick}>
             <View style={styles.container}>
-                <Animated.Text style={[styles.descricao, { transform: [{ scale: bounceValue }] }]}>{descricao}</Animated.Text>
-                <Text style={styles.cartas}>{qtdCartas > 0 ? qtdCartas + ' carta(s)' : 'Nenhuma carta cadastrada'}</Text>
+                <Animated.Text style={[styles.descricaoBaralho, { transform: [{ scale: bounceValue }] }]}>{descricao}</Animated.Text>
+                <Text style={styles.qtdCartas}>{qtdCartas > 0 ? qtdCartas + ' carta(s)' : 'Nenhuma carta cadastrada'}</Text>
             </View>
         </TouchableOpacity>
     )
@@ -26,18 +26,19 @@ export default function Baralho({ dados, exibirBaralho }) {
 
 const styles = StyleSheet.create({
     container: {
-        margin: 5,
+        margin: 3,
         padding: 5,
-        backgroundColor: 'white',
-        borderBottomColor: 'gray',
-        borderBottomWidth: 1,
+        backgroundColor: '#F3F3F3',
+        borderColor: '#A6A7A8',
+        borderWidth: 0.5,
+        borderRadius: 4,
     },
-    descricao: {
+    descricaoBaralho: {
         textAlign: 'center',
         fontWeight: 'bold',
         fontSize: 18,
     },
-    cartas: {
+    qtdCartas: {
         textAlign: 'center',
         fontSize: 14,
         color: 'gray'
