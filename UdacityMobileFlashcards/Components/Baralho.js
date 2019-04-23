d58d95806e0ad23d39c066b92244cd3485030d2e
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2019-04-22 21:49:23 -0300

    feat: Requisito do projeto de animação ao clicar em um baralho na listagem de baralhos

--- UdacityMobileFlashcards/Components/Baralho.js
+++ UdacityMobileFlashcards/Components/Baralho.js
@@ -1,13 +1,23 @@
 import React from 'react'
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity, Animated } from 'react-native'
 
 export default function Baralho({ dados, exibirBaralho }) {
     const { descricao, qtdCartas, id } = dados
+    let bounceValue = new Animated.Value(1)
+
+    onBaralhoClick = () => {
+
+        // Anima o título do baralho e navega para o detalhe da mesma
+        Animated.sequence([
+            Animated.timing(bounceValue, { duration: 100, toValue: 1.04 }),
+            Animated.spring(bounceValue, { toValue: 1, friction: 4 })
+        ]).start(() => exibirBaralho(id))
+    }
 
     return (
-        <TouchableOpacity onPress={() => exibirBaralho(id)}>
+        <TouchableOpacity onPress={onBaralhoClick}>
             <View style={styles.container}>
-                <Text style={styles.descricao}>{descricao}</Text>
+                <Animated.Text style={[styles.descricao, { transform: [{ scale: bounceValue }] }]}>{descricao}</Animated.Text>
                 <Text style={styles.cartas}>{qtdCartas > 0 ? qtdCartas + ' carta(s)' : 'Nenhuma carta cadastrada'}</Text>
             </View>
         </TouchableOpacity>
