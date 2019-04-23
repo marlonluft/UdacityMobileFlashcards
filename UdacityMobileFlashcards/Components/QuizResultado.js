80d285fbb6d94ba3681173c63d065c32ccc43f96
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2019-04-22 23:34:33 -0300

    feat: Estilo quiz resultado

--- UdacityMobileFlashcards/Components/QuizResultado.js
+++ UdacityMobileFlashcards/Components/QuizResultado.js
@@ -1,15 +1,55 @@
 import React from 'react'
-import { View, Text, StyleSheet, Button } from 'react-native'
+import { View, Text, StyleSheet, TouchableOpacity } from 'react-native'
+import defaultStyles from '../Utils/Style'
 
-export default function QuizResultado({qtdAcertos, qtdTotal, recomecarQuiz, voltarBaralho}) {
+export default function QuizResultado({ qtdAcertos, qtdTotal, recomecarQuiz, voltarBaralho }) {
+
+    getMensagem = () => {
+
+        var porcentagem = (qtdAcertos * 100) / qtdTotal
+
+        if (porcentagem === 100) {
+            return 'Acertou todas as perguntas! Você é um(a) deus(a) do conhecimento 🤩'
+        }
+        else if (porcentagem >= 70) {
+            return 'Bahh ' + porcentagem + '% de acerto, ai sim ein 👏👏👏'
+        }
+        else if (porcentagem >= 50) {
+            return porcentagem + '% de acertos, na próxima vai! 🤨'
+        }
+        else if (porcentagem > 0) {
+            return 'Você acertou ' + porcentagem + '% das pergunta(s) 😅'
+        }
+        else {
+            return 'Vish, acho que bugou aqui, deu ' + porcentagem + '% de acerto 🤔'
+        }
+    }
 
     return (
         <View>
 
-            <Text>Você acertou {qtdAcertos} de {qtdTotal} pergunta(s)!</Text>
+            <Text style={styles.resultado}>{getMensagem()}</Text>
 
-            <Button onPress={() => recomecarQuiz()} title="Recomeçar quiz" />
-            <Button onPress={() => voltarBaralho()} title="Voltar ao baralho" />
+            <TouchableOpacity
+                style={defaultStyles.touchableOpacity}
+                onPress={() => recomecarQuiz()}>
+                <Text style={defaultStyles.touchableOpacityText}>Recomeçar quiz</Text>
+            </TouchableOpacity>
+            <TouchableOpacity
+                style={defaultStyles.touchableOpacity}
+                onPress={() => voltarBaralho()}>
+                <Text style={defaultStyles.touchableOpacityText}>Voltar ao baralho</Text>
+            </TouchableOpacity>
         </View>
     )
 }
+
+const styles = StyleSheet.create({
+    resultado: {
+        textAlign: 'center',
+        fontSize: 20,
+        fontWeight: 'bold',
+        color: 'gray',
+        marginVertical: 10,
+    },
+})
