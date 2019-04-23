4a281d5fe6645563aefc62b5c29cf9c8f48c5298
Marlon Luft Salvador <marlon-luft-salvador@hotmail.com>
2019-04-22 22:27:27 -0300

    feat: Criado arquivo para estilização padrão do app

---
+++ UdacityMobileFlashcards/Utils/Style.js
@@ -0,0 +1,19 @@
+import { StyleSheet } from 'react-native'
+
+export default defaultStyles = StyleSheet.create({
+    container: {
+        flex: 1,
+        paddingHorizontal: 10,
+        backgroundColor: 'white'
+    },
+    nenhumCadastrado: {
+        textAlign: 'center',
+        color: 'gray'
+    },
+    button: {
+        alignItems: 'center',
+        backgroundColor: '#DDDDDD',
+        padding: 10,
+        marginVertical: 10,
+    }
+})
